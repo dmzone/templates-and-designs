2347084333a95262cf9f23e716753012755e3864
alexswiontek <contact@alexswan.tech>
2016-02-23 11:44:56 -0800

    feat: Add a gulp workflow template

---
+++ Gulp Workflow Template/gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    critical = require('critical'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    htmlmin = require('gulp-htmlmin');
+
+gulp.task('styles', function(){
+  return gulp.src('src/css/*.css')
+    .pipe(cssnano({discardComments: {removeAll: true}}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function(){
+  return gulp.src('src/img/*')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html', function(){
+  return gulp.src('src/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('copy-paste', function() {
+  gulp.src('src/uploads/*')
+    .pipe(gulp.dest('dist/uploads'));
+});
+
+gulp.task('critical', function (cb) {
+  critical.generate({
+      inline: true,
+      src: 'index.html',
+      dest: 'dist/index.html',
+      width: 1300,
+      height: 900
+  });
+});
+
+gulp.task('default', [
+  'styles', 'scripts','images', 'html', 'copy-paste'
+  ]
+);
