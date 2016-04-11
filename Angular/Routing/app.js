2ee0c521413e4d1328f06029a88313bad3db5a06
alexswiontek <contact@alexswan.tech>
2016-04-10 18:23:25 -0700

    feat: implement API functionality to router app

--- Angular/Routing/app.js
+++ Angular/Routing/app.js
@@ -16,9 +16,14 @@
 			controller: 'secondController'
 		})
 		
+		.when('/api', {
+			templateUrl: 'templates/api.html',
+			controller: 'apiController',
+			controllerAs: 'results'
+		})
+		
 		.when('/404', {
-			templateUrl: 'templates/error.html',
-			controller: 'errorController'
+			templateUrl: 'templates/error.html'
 		})
 		
 		.otherwise({redirectTo: '/404'});
@@ -39,8 +44,14 @@
 		$scope.name = 'Second';
 	}])
 		
-	.controller('errorController', ['$scope', '$log', function($scope, $log) {
-		$scope.name = 'Help me!';
+	.controller('apiController', ['$http', function($http) {
+		var controller = this;
+		
+		$http.get('https://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com').success(function(listResults, status){
+			controller.properties = listResults;
+			controller.status = status;
+		});
+		
 	}]);		
 		
 })();
